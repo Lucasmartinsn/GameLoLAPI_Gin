fba6bd1ed848794328a38ff2b1af69bacfa0904f
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-14 11:34:27 -0300

    package models

---
+++ models/entities.go
@@ -0,0 +1,9 @@
+package models
+
+type Record struct {
+	Id_user       int    `json:"id_user"`
+	Foto          string `json:"foto"`
+	Nome_ususario int    `json:"nome_usuario"`
+	Email         int    `json:"email"`
+	Senha         int    `json:"senha"`
+}

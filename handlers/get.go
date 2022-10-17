870faf0ee94c3d3a1fc1c9ba97744f1bb78fef20
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-17 11:53:53 -0300

    get unitary, working

--- handlers/get.go
+++ handlers/get.go
@@ -1,19 +1,28 @@
 package handlers
 
 import (
-	"log"
+	"strconv"
 
 	"github.com/Lucasmartinsn/new-api-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
-func Get(c *gin.Context) {
-	resp, err := models.GetAll()
+func GetOne(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
-		log.Fatalln(err)
-	} else {
-		c.JSON(200, resp)
+		c.JSON(400, gin.H{
+			"Error": "id tem de ser um int",
+		})
+		return
 	}
 
+	register, err := models.GetOne(int(id))
+	if err != nil {
+		c.JSON(400, gin.H{
+			"Error": "geristro nao existe",
+		})
+		return
+	}
+	c.JSON(200, register)
 }

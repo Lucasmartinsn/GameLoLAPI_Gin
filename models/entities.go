7f9636a9da608ea4723a02d2c6fe3ec0d5bd769e
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-18 11:12:48 -0300

    refatoração

--- models/entities.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Record struct {
-	Id_user       int    `json:"id_user"`
-	Foto          string `json:"foto"`
-	Nome_ususario string `json:"nome_usuario"`
-	Email         string `json:"email"`
-	Senha         int    `json:"senha"`
-}

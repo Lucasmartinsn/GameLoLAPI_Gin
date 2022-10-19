cbefa49aee585854bb04d5c98ea79878b46cf7a6
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-19 10:45:52 -0300

    server call

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
-/*
 import (
 	"github.com/Lucasmartinsn/new-api-gin/configs"
-	"github.com/Lucasmartinsn/new-api-gin/handles"
-	"github.com/gin-gonic/gin"
+	"github.com/Lucasmartinsn/new-api-gin/server"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/cadastros", handles.Getcadastros)
-	router.GET("/cadastros/:id", handles.GetCadastroByID)
-	router.POST("/cadastros", handles.PostCadastro)
-	router.PUT("/cadastros/:id", handles.UpdateCadastroById)
-	router.DELETE("/cadastos/:id", handles.DeleteCadastroById)
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+	server := server.NewServer()
 
-	router.Run(configs.GetServerPort())
-}*/
+	server.Run()
+}

fad3d893159cc64faa3eaefdd25e227079176324
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-20 11:42:21 -0300

    framework for updating just the photo

---
+++ models/records/update_foto.go
@@ -0,0 +1,21 @@
+package models
+
+import "github.com/Lucasmartinsn/new-api-gin/db"
+
+func Upfoto(id int64, records Record) (int64, error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return 0, err
+	}
+
+	defer conn.Close()
+
+	response, err := conn.Exec(`UPDATE records SET foto=$2 WHERE id=$1`,
+		id, records.Foto)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return response.RowsAffected()
+}

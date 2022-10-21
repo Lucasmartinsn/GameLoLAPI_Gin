70cac5eeb7bf83f36631f98a7e21fc7cbea50310
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-21 11:48:16 -0300

    adding field that was missing

--- models/info_game/insert_info.go
+++ models/info_game/insert_info.go
@@ -8,9 +8,9 @@ func Insertinfo(infogame Infogame) (id int, err error) {
 		return
 	}
 
-	sql := `INSERT INTO user_game_infor (nick_game, days_play, position_game, play_time, comunication) 	VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`
+	sql := `INSERT INTO user_game_infor (nick_game, descricao, days_play, position_game, play_time, comunication) 	VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`
 
-	err = conn.QueryRow(sql, infogame.Nick_game, infogame.Days_play, infogame.Position_game, infogame.Play_time, infogame.Comunication).Scan(&id)
+	err = conn.QueryRow(sql, infogame.Nick_game, infogame.Descricao, infogame.Days_play, infogame.Position_game, infogame.Play_time, infogame.Comunication).Scan(&id)
 
 	defer conn.Close()
 
